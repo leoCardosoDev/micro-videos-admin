aa9426fc2cd9d6f328414ab0e7bacfd4d6c40d02
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-09 12:32:21 -0300

    feat: add sequilize type script

--- src/category/infra/db/in-memory/category-in-memory.repository.spec.ts
+++ src/category/infra/db/in-memory/category-in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
-import { Category } from '../../domain/category.entity';
+import { Category } from '../../../domain/category.entity';
 import { CategoryInMemoryRepository } from './category-in-memory.repository';
+
 
 describe('CategoryInMemoryRepository', () => {
   let repository: CategoryInMemoryRepository
