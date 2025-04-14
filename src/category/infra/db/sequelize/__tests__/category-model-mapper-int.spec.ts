e99cf5a24757b0a088c38ba6f5029c999485ba0d
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-14 17:57:49 -0300

    feat: add adjust tests to sequileze

--- src/category/infra/db/sequelize/__tests__/category-model-mapper-int.spec.ts
+++ src/category/infra/db/sequelize/__tests__/category-model-mapper-int.spec.ts
@@ -1,22 +1,12 @@
-import { Sequelize } from 'sequelize-typescript';
 import { CategoryModel } from '../category.model';
 import { CategoryModelMapper } from '../category-model-mapper';
 import { EntityValidationError } from '../../../../../shared/domain/validators/validation.error';
 import { Category } from '../../../../domain/category.entity';
-import { UUID } from 'sequelize';
 import { Uuid } from '../../../../../shared/domain/value-objects/uuid.vo';
+import { setupSequelize } from '../../../../../shared/infra/testing/helpers';
 
 describe('CategoryModelMapper Integration Test', () => {
-  let sequelize: Sequelize
-    beforeEach(async () => {
-      sequelize = new Sequelize({
-        dialect: 'sqlite',
-        storage: ':memory:',
-        logging: false,
-        models: [CategoryModel],
-      })
-      await sequelize.sync({ force: true })
-    })
+  setupSequelize({ models: [CategoryModel] });
 
   it('should throws error when category is invalid', async () => {
     const model = CategoryModel.build({
