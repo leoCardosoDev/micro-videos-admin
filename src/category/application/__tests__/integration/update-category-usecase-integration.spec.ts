49735b80bb61d0474d6b2825715414b1df4dc855
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-16 14:37:47 -0300

    test: add update UpdateCategoryUseCase integration tests

---
+++ src/category/application/__tests__/integration/update-category-usecase-integration.spec.ts
@@ -0,0 +1,43 @@
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/helpers";
+import { CategorySequelizeRepository } from "../../../infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "../../../infra/db/sequelize/category.model";
+import { UpdateCategoryUseCase } from "../../update-category-usecase";
+import { NotFoundError } from "../../../../shared/domain/erros/not.found.error";
+import { Category } from "../../../domain/category.entity";
+
+describe('UpdateCategoryUseCase Integration Tests', () => {
+  let useCase: UpdateCategoryUseCase
+  let repository: CategorySequelizeRepository
+  setupSequelize({models: [CategoryModel]})
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel)
+    useCase = new UpdateCategoryUseCase(repository)
+  })
+
+  it('should throws error when entity not found', async () => {
+    const uuid = new Uuid()
+    await expect(() =>
+      useCase.execute({ id: uuid.id, name: 'fake' }))
+      .rejects.toThrow(new NotFoundError(uuid.id, Category))
+  })
+
+  it('should update a category', async () => {
+    const entity = Category.fake().aCategory().build()
+    await repository.insert(entity)
+    let output = await useCase.execute({
+      id: entity.category_id.id,
+      name: 'test',
+      description: 'some description',
+      is_active: true,
+    })
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: 'test',
+      description:'some description',
+      is_active: true,
+      created_at: entity.created_at,
+    })
+  })
+})
