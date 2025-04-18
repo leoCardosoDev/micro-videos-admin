17981b08a398a80d0c201526ee23396d1b400bd9
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2025-04-18 15:59:04 -0300

    feat: add list category and output

---
+++ src/shared/application/pagination-output.ts
@@ -0,0 +1,24 @@
+import { SearchResult } from "../domain/repository/search.result"
+
+export type PaginationOutput<T> = {
+  items: T[]
+  total: number
+  current_page: number
+  last_page: number
+  per_page: number
+}
+
+export class PaginationOutputMapper {
+  static toOutput<T>(
+    items: T[],
+    props: Omit<SearchResult, "items">,
+  ): PaginationOutput<T> {
+    return {
+      items,
+      total: props.total,
+      current_page: props.current_page,
+      last_page: props.last_page,
+      per_page: props.per_page,
+    }
+  }
+}
